50ce4fda592e5e269938618c36afa9e860cc9d83
rcwaterman <134216651+rcwaterman@users.noreply.github.com>
2025-03-26 19:08:00 -0400

    Append working with unpack feature, TODO: get insert working with unpack and add a remove feature.

--- arrays/array.py
+++ arrays/array.py
@@ -12,26 +12,31 @@ class Array():
     Let's implement these.
     """
 
-    def __init__(self, element=None):
+    def __init__(self, element=None, unpack=True):
         """
         We will probably want this to init with whatever is passed into the array as a valid element.
 
         If nothing is passed to the array, initialize with the capacity for one element.
+
+        By default, the array will unpack lists or other arrays that are initialized within it.
         """
 
         self.length = 0
-        self.elements = 1
+        self.elements = 0
         self.array = self.allocate_array()
 
         # if there is an element passed, see if it is iterable, otherwise, handle it
         if element is not None:
             #check if the element is a list... if so, unpack into our array
-            if isinstance(element,list):
-                self.array = self.allocate_array(len(element))
-                for i in range(len(element)):
-                    self.array[i] = element[i]
-                    self.length+=1
-            #otherwise, dump the element into an element
+            if isinstance(element,list) or isinstance(element,Array):
+                if unpack:
+                    self.array = self.allocate_array(len(element))
+                    for i in range(len(element)):
+                        self.array[i] = element[i]
+                        self.length+=1
+                #otherwise, dump the element into an element
+                else:
+                    self.append(element, unpack=False)
             else:
                 self.append(element)
 
@@ -51,9 +56,12 @@ class Array():
     
     def __str__(self):
         array_str = "["
-        for i in range(len(self.array)):
-            array_str = array_str + str(self.array[i]) + ', '
-        array_str = array_str[:-2] + ']'
+        if len(self.array):
+            for i in range(len(self.array)):
+                array_str = array_str + str(self.array[i]) + ', '
+            array_str = array_str[:-2] + ']'
+        else:
+            array_str = array_str + ']'
         return array_str
 
     def allocate_array(self, element_count=None):
@@ -74,32 +82,61 @@ class Array():
         self.array = temp
         self.length = new_element_count
 
-    def append(self, element):
+    def append(self, element, unpack=False):
         """
         This function will always take in an element and assign the index based on the size of the existing array. 
 
         It is important to note that the index of the last element will always be 1-length.
 
-        This also needs to handle list inputs.
+        This also needs to handle list inputs, which will be unpacked based on the unpack keyword.
         """
         
         #check if the element is a list... if so, unpack into our array
-        if isinstance(element,list):
-            self.original_length = self.length
-            self._resize(self.length + len(element))
-            for i in range(len(element)):
-                self.array[self.original_length+i] = element[i]
+        if isinstance(element,list) or isinstance(element,Array):
+            if unpack:
+                self.original_length = self.length
+                self._resize(self.length + len(element))
+                for i in range(len(element)):
+                    self.array[self.original_length+i] = element[i]
+            else:
+                self.length+=1
+                self._resize(self.length)
+                self.array[self.length-1] = element
         else:
             self.length+=1
             self._resize(self.length)
             self.array[self.length-1] = element
 
-    def prepend(self, element):
+    def insert(self, element=None, index=0, unpack=False):
         """
-        Place the element at index 0 of list.   
+        Place the element into the array at a given index.
+
+        The default behavior is to place the element at index 0.
+
+        The unpack keyword will unpack the element at the index if it is a list.
         """
-        self.length+=1
-        new_array = self.allocate_array()
+
+        # I want to simulate how this would be done at the lowest level, which is to inject the value
+        # at index zero, then shift all subsequent values
+
+        #check if the element is a list... if so, unpack into our array
+        if isinstance(element,list) or isinstance(element,Array):
+            if unpack:
+                self.original_length = self.length
+                self._resize(self.length + len(element))
+                for i in range(len(element)):
+                    # The value should only be inserted if we are at the defined index
+                    if i < index + len(element):
+                        self.array[self.original_length+i] = element[i]
+            else:
+                self.length+=1
+                self._resize(self.length)
+                self.array[self.length-1] = element
+            
+        else:
+            self.length+=1
+            self._resize(self.length)
+            self.array[self.length-1] = element
 
     def pop(self, index=None):
         """
@@ -122,13 +159,11 @@ class Array():
                 self._resize(self.length-1)
 
 if __name__ == '__main__':
-    array = Array([(1,2,3,4,5,6), (1,2,3,4,5,6)])
-    array.append([2,3,4,5,6])
-    print(array, len(array))
-    array.pop(0)
-    print(array, len(array))
-    array.pop(2)
-    print(array, len(array))
-    array.append([2,3,4,5,6])
-    print(array, len(array))
-    array.pop()
+    array1 = Array([(1,2,3,4,5,6), (1,2,3,4,5,6)], unpack=False)
+    print(array1, len(array1))
+    array1.insert([6,7,8,9,10], unpack=True)
+    print(array1, len(array1))
+    array2 = Array(2)
+    print(array2, len(array2))
+    array1.append(array2)
+    print(array1, len(array1))
